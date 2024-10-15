f721d0749d747e18673c72df24b27cce5b499447
Femi Akinsiku <akinsiku13@gmail.com>
2024-10-15 15:55:49 +0100

    updates

--- libs/products/src/lib/products.tsx
+++ libs/products/src/lib/products.tsx
@@ -1,9 +1,11 @@
 import styles from './products.module.css';
 
+
 export function Products() {
   return (
     <div className={styles['container']}>
       <h1>Welcome to Products!</h1>
+      <p>This is a change</p>
     </div>
   );
 }
