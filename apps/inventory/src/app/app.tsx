da1135dbe1f001a30417222d0d752a52ed210edb
Femi Akinsiku <akinsiku13@gmail.com>
2024-10-15 14:45:13 +0100

    initial commit

---
+++ apps/inventory/src/app/app.tsx
@@ -0,0 +1,14 @@
+// eslint-disable-next-line @typescript-eslint/no-unused-vars
+import styles from './app.module.css';
+
+import { Products } from '@react-monorepo/products';
+
+export function App() {
+  return (
+    <div>
+      <Products />
+    </div>
+  );
+}
+
+export default App;
